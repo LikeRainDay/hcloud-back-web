da3120c83b653b8bdef6f4c2f57d3f3adf46c173
houshuai <houshuai>
2019-04-02 09:10:03 +0800

    add minator component

---
+++ src/app/pages/minator/minator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-minator',
+  templateUrl: './minator.component.html',
+  styleUrls: ['./minator.component.scss']
+})
+export class MinatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
